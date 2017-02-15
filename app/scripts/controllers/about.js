599541b45d484345a31c602406e43ff7177490a4
Naveen Santhosh <mr.naveensanthosh@gmail.com>
2017-02-15 11:47:44 +0530

    First commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name gmasAssignmentApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the gmasAssignmentApp
+ */
+angular.module('gmasAssignmentApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name gmasAssignmentApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the gmasAssignmentApp
+ */
+angular.module('gmasAssignmentApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
